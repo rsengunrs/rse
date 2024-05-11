e64adc7e958ffcd8b2ad2ecac7a6238a1460c0c9
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:32:01 +0300

    Initialize penalosa-patch-1

---
+++ fixtures/node-app-pages/tests/index.test.ts
@@ -0,0 +1,55 @@
+import { fork } from "child_process";
+import path from "path";
+import { fetch } from "undici";
+import type { ChildProcess } from "child_process";
+
+describe("Pages Dev", () => {
+	let wranglerProcess: ChildProcess;
+	let ip: string;
+	let port: number;
+	let resolveReadyPromise: (value: unknown) => void;
+	const readyPromise = new Promise((resolve) => {
+		resolveReadyPromise = resolve;
+	});
+
+	beforeAll(() => {
+		wranglerProcess = fork(
+			path.join("..", "..", "packages", "wrangler", "bin", "wrangler.js"),
+			["pages", "dev", "public", "--node-compat", "--port=0"],
+			{
+				stdio: ["inherit", "inherit", "inherit", "ipc"],
+				cwd: path.resolve(__dirname, ".."),
+			}
+		).on("message", (message) => {
+			const parsedMessage = JSON.parse(message.toString());
+			ip = parsedMessage.ip;
+			port = parsedMessage.port;
+			resolveReadyPromise(undefined);
+		});
+	});
+
+	afterAll(async () => {
+		await new Promise((resolve, reject) => {
+			wranglerProcess.once("exit", (code) => {
+				if (!code) {
+					resolve(code);
+				} else {
+					reject(code);
+				}
+			});
+			wranglerProcess.kill("SIGTERM");
+		});
+	});
+
+	it.concurrent(
+		"should work with `--node-compat` when running code requiring polyfills",
+		async () => {
+			await readyPromise;
+			const response = await fetch(`http://${ip}:${port}/stripe`);
+
+			await expect(response.text()).resolves.toContain(
+				`"PATH":"path/to/some-file","STRIPE_OBJECT"`
+			);
+		}
+	);
+});
---
+++ fixtures/remix-pages-app/tests/index.test.ts
@@ -0,0 +1,56 @@
+import { fork, spawnSync } from "child_process";
+import * as path from "path";
+import { fetch } from "undici";
+import type { ChildProcess } from "child_process";
+
+const isWindows = process.platform === "win32";
+
+describe("Remix", () => {
+	let wranglerProcess: ChildProcess;
+	let ip: string;
+	let port: number;
+	let resolveReadyPromise: (value: unknown) => void;
+	const readyPromise = new Promise((resolve) => {
+		resolveReadyPromise = resolve;
+	});
+
+	beforeAll(async () => {
+		spawnSync("npm", ["run", "build"], {
+			shell: isWindows,
+			cwd: path.resolve(__dirname, ".."),
+		});
+		wranglerProcess = fork(
+			path.join("..", "..", "packages", "wrangler", "bin", "wrangler.js"),
+			["pages", "dev", "public", "--port=0"],
+			{
+				stdio: ["inherit", "inherit", "inherit", "ipc"],
+				cwd: path.resolve(__dirname, ".."),
+			}
+		).on("message", (message) => {
+			const parsedMessage = JSON.parse(message.toString());
+			ip = parsedMessage.ip;
+			port = parsedMessage.port;
+			resolveReadyPromise(undefined);
+		});
+	});
+
+	afterAll(async () => {
+		await new Promise((resolve, reject) => {
+			wranglerProcess.once("exit", (code) => {
+				if (!code) {
+					resolve(code);
+				} else {
+					reject(code);
+				}
+			});
+			wranglerProcess.kill("SIGTERM");
+		});
+	});
+
+	it.concurrent("renders", async () => {
+		await readyPromise;
+		const response = await fetch(`http://${ip}:${port}/`);
+		const text = await response.text();
+		expect(text).toContain("Welcome to Remix");
+	});
+});
