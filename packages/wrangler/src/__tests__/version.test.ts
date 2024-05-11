034c669d983f9bbca7ad77b63a26c4bf2f9a704e
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:47:37 +0300

    Initialize workerd-CODE_MOVED

---
+++ packages/wrangler/src/__tests__/version.test.ts
@@ -0,0 +1,49 @@
+import { version } from "./../../package.json";
+import { mockConsoleMethods } from "./helpers/mock-console";
+import { useMockIsTTY } from "./helpers/mock-istty";
+import { runWrangler } from "./helpers/run-wrangler";
+
+describe("version", () => {
+	const std = mockConsoleMethods();
+	const { setIsTTY } = useMockIsTTY();
+
+	// We cannot test output of version banner,
+	// as it is disabled in jest environments
+
+	// it("should output version banner", async () => {
+	// 	await runWrangler("-v");
+	// 	expect(std.out).toMatchInlineSnapshot(`
+	// 	" ⛅️ wrangler 2.0.22
+	// 	[38;5;214m--------------------[39m"
+	// `);
+	// });
+
+	it("should output current version if !isTTY calling with `-v` flag", async () => {
+		setIsTTY(false);
+
+		await runWrangler("-v");
+		expect(std.out).toMatch(version);
+		expect(std.warn).toBe("");
+	});
+
+	// This run separately as command handling is different
+	it("should output current version if !isTTY calling with `--version` flag", async () => {
+		setIsTTY(false);
+
+		await runWrangler("--version");
+		expect(std.out).toMatch(version);
+		expect(std.warn).toBe("");
+	});
+
+	it("should output current version if !isTTY calling (deprecated) `version` command", async () => {
+		setIsTTY(false);
+
+		await runWrangler("version");
+		expect(std.out).toMatch(version);
+		expect(std.warn).toMatchInlineSnapshot(`
+		"[33m▲ [43;33m[[43;30mWARNING[43;33m][0m [1m\`wrangler version\` is deprecated and will be removed in a future major version. Please use \`wrangler --version\` instead.[0m
+
+		"
+	`);
+	});
+});
