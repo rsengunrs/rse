d872aa0fe8b30be9228df097f80dcc4e9964c489
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:44:21 +0300

    Initialize v4

---
+++ packages/eslint-config-worker/index.js
@@ -0,0 +1,82 @@
+// Settings here should be considered Global ESLint settings for the monorepo. We don't need to define --config in each package.
+// https://eslint.org/docs/latest/use/configure/
+// https://eslint.org/docs/latest/use/configure/configuration-files#using-configuration-files
+// This can be in the form of a .eslintrc.* file or an eslintConfig field in a package.json file,
+// both of which ESLint will look for and read automatically, or you can specify a configuration file on the command line.
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+	ignorePatterns: [
+		"**/node_modules/**",
+		"examples",
+		"**/templates/**/*.*",
+		".eslintrc.js",
+		"**/dist/**",
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: 2022,
+		sourceType: "module",
+		project: true,
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
+	plugins: [
+		"@typescript-eslint",
+		"eslint-plugin-react",
+		"eslint-plugin-react-hooks",
+		"import",
+		"unused-imports",
+		"no-only-tests",
+	],
+	extends: ["turbo"],
+	overrides: [
+		{
+			files: ["*.ts", "*.tsx"],
+			extends: [
+				"eslint:recommended",
+				"plugin:@typescript-eslint/recommended",
+				"plugin:react/recommended",
+				"plugin:react-hooks/recommended",
+				"plugin:import/typescript",
+				"turbo",
+			],
+
+			rules: {
+				"no-empty": "off",
+				"no-empty-function": "off",
+				"no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+				"no-only-tests/no-only-tests": "error",
+				"require-yield": "off",
+				"@typescript-eslint/consistent-type-imports": ["error"],
+				"@typescript-eslint/no-empty-function": "off",
+				"@typescript-eslint/no-explicit-any": "error",
+				"@typescript-eslint/no-floating-promises": "error",
+				"@typescript-eslint/no-non-null-assertion": "error",
+				"no-shadow": "off",
+				"@typescript-eslint/no-shadow": "error",
+				"no-unused-vars": "off",
+				"@typescript-eslint/no-unused-vars": [
+					"error",
+					{
+						argsIgnorePattern: ".*",
+						varsIgnorePattern: "^_",
+						ignoreRestSiblings: true,
+					},
+				],
+				"unused-imports/no-unused-imports": "error",
+				"unused-imports/no-unused-vars": [
+					"error",
+					{
+						vars: "all",
+						varsIgnorePattern: "^_",
+						args: "after-used",
+						argsIgnorePattern: "^_",
+					},
+				],
+			},
+		},
+	],
+};
---
+++ packages/vitest-pool-workers/CHANGELOG.md
@@ -0,0 +1,29 @@
+# @cloudflare/vitest-pool-workers
+
+## 0.1.2
+
+### Patch Changes
+
+- Updated dependencies [[`a0768bc`](https://github.com/cloudflare/workers-sdk/commit/a0768bcc9d76be8a88fe3e1aa45f3b3805da3df6)]:
+  - wrangler@3.34.2
+
+## 0.1.1
+
+### Patch Changes
+
+- Updated dependencies [[`2e50d51`](https://github.com/cloudflare/workers-sdk/commit/2e50d51632dfe905bd32de8176231bb256c88dab)]:
+  - miniflare@3.20240304.2
+  - wrangler@3.34.1
+
+## 0.1.0
+
+### Minor Changes
+
+- [#5241](https://github.com/cloudflare/workers-sdk/pull/5241) [`ca891e7`](https://github.com/cloudflare/workers-sdk/commit/ca891e783a41053770f278b91f9001c477550743) Thanks [@mrbbot](https://github.com/mrbbot)! - feature: implement Workers Vitest integration
+
+  Introducing the new Workers Vitest integration! The `@cloudflare/vitest-pool-workers` package allows you to write unit and integration tests using Vitest that run inside the Workers runtime. Refer to the [documentation](https://developers.cloudflare.com/workers/testing/vitest-integration/) and [examples](https://github.com/cloudflare/workers-sdk/tree/main/fixtures/vitest-pool-workers-examples/) for more information.
+
+### Patch Changes
+
+- Updated dependencies [[`03484c2`](https://github.com/cloudflare/workers-sdk/commit/03484c2d64f42a2820feeec9076dc3f210baf4f9), [`29e8151`](https://github.com/cloudflare/workers-sdk/commit/29e8151bc2235bd13074584df5f90187955123d2), [`4730b6c`](https://github.com/cloudflare/workers-sdk/commit/4730b6c087080d79838d3fd86480d8aff693834a), [`bd935cf`](https://github.com/cloudflare/workers-sdk/commit/bd935cfdf1bebfff53b1817d475b1d36eccec9c0)]:
+  - wrangler@3.34.0
