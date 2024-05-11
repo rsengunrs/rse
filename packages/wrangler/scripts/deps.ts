346a62d124dd2da5d11fa00147296480d9de8917
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:41:54 +0300

    Initialize test-4775

---
+++ packages/wrangler/scripts/deps.ts
@@ -0,0 +1,30 @@
+import fs from "node:fs";
+import path from "node:path";
+
+/**
+ * Dependencies that _are not_ bundled along with wrangler
+ */
+export const EXTERNAL_DEPENDENCIES = [
+	"fsevents",
+	"esbuild",
+	"blake3-wasm",
+	"miniflare",
+	// todo - bundle miniflare too
+	"selfsigned",
+	"source-map",
+	"@esbuild-plugins/node-globals-polyfill",
+	"@esbuild-plugins/node-modules-polyfill",
+	"chokidar",
+];
+
+const pathToPackageJson = path.resolve(__dirname, "..", "package.json");
+const packageJson = fs.readFileSync(pathToPackageJson, { encoding: "utf-8" });
+const { dependencies, devDependencies } = JSON.parse(packageJson);
+
+/**
+ * Dependencies that _are_ bundled along with wrangler
+ */
+export const BUNDLED_DEPENDENCIES = Object.keys({
+	...dependencies,
+	...devDependencies,
+}).filter((dep) => !EXTERNAL_DEPENDENCIES.includes(dep));
