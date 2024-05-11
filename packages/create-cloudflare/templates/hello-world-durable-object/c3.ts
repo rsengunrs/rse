21903bf09dec99d7e92075682dd17d206ea0a570
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:48:29 +0300

    Initialize elithrar-patch-1

---
+++ packages/create-cloudflare/templates/hello-world-durable-object/c3.ts
@@ -0,0 +1,16 @@
+export default {
+	configVersion: 1,
+	id: "hello-world-durable-object",
+	displayName: "Co-ordination / multiplayer API (using Durable Objects)",
+	platform: "workers",
+	copyFiles: {
+		variants: {
+			js: {
+				path: "./js",
+			},
+			ts: {
+				path: "./ts",
+			},
+		},
+	},
+};
