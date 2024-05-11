2f7e727115c23de9be91c78a12adcb730c98e3a5
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:44:43 +0300

    Initialize dependabot/npm_and_yarn/packages/create-cloudflare/src/frameworks/create-qwik-1.5.1

---
+++ packages/wrangler/src/ai/fetcher.ts
@@ -0,0 +1,24 @@
+import { Headers, Response } from "miniflare";
+import { performApiFetch } from "../cfetch/internal";
+import { getAccountId } from "../user";
+import type { Request } from "miniflare";
+
+export async function AIFetcher(request: Request) {
+	const accountId = await getAccountId();
+
+	request.headers.delete("Host");
+	request.headers.delete("Content-Length");
+
+	const res = await performApiFetch(`/accounts/${accountId}/ai/run/proxy`, {
+		method: "POST",
+		headers: Object.fromEntries(request.headers.entries()),
+		body: request.body,
+		duplex: "half",
+	});
+
+	const respHeaders = new Headers(res.headers);
+	respHeaders.delete("Host");
+	respHeaders.delete("Content-Length");
+
+	return new Response(res.body, { status: res.status, headers: respHeaders });
+}
