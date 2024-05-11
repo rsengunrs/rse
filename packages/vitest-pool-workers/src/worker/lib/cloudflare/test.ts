ea618ed6b4a132a07ab4a5e9e3beb527cb04cbb2
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:47:59 +0300

    Initialize test-for-emfile

---
+++ packages/vitest-pool-workers/src/worker/lib/cloudflare/test.ts
@@ -0,0 +1,14 @@
+export {
+	env,
+	SELF,
+	fetchMock,
+	runInDurableObject,
+	runDurableObjectAlarm,
+	listDurableObjectIds,
+	createExecutionContext,
+	waitOnExecutionContext,
+	createScheduledController,
+	createMessageBatch,
+	getQueueResult,
+	applyD1Migrations,
+} from "cloudflare:test-internal";
