7d365202f4c96a0dbf1141127a18d6fb66416f48
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:33:34 +0300

    Initialize pages-publish-fn-build

---
+++ packages/pages-shared/__tests__/asset-server/handler.test.ts
@@ -0,0 +1,528 @@
+import { Cache } from "@miniflare/cache";
+import { MemoryStorage } from "@miniflare/storage-memory";
+import {
+	generateHandler,
+	getResponseFromMatch,
+} from "../../asset-server/handler";
+import { createMetadataObject } from "../../metadata-generator/createMetadataObject";
+import type { HandlerContext } from "../../asset-server/handler";
+import type { Metadata } from "../../asset-server/metadata";
+import type { RedirectRule } from "../../metadata-generator/types";
+
+describe("asset-server handler", () => {
+	test("Returns appropriate status codes", async () => {
+		const statuses = [301, 302, 303, 307, 308];
+		const metadata = createMetadataObjectWithRedirects(
+			statuses
+				.map((status) => ({
+					status,
+					from: `/${status}`,
+					to: "/home",
+				}))
+				.concat({
+					status: 302,
+					from: "/500",
+					to: "/home",
+				})
+		);
+
+		const tests = statuses.map(async (status) => {
+			const { response } = await getTestResponse({
+				request: "https://foo.com/" + status,
+				metadata,
+			});
+
+			expect(response.status).toBe(status);
+			expect(response.headers.get("Location")).toBe("/home");
+		});
+
+		await Promise.all(tests);
+
+		const { response } = await getTestResponse({
+			request: "https://foo.com/500",
+			metadata,
+		});
+
+		expect(response.status).toBe(302);
+		expect(response.headers.get("Location")).toBe("/home");
+	});
+
+	test("Match exact pathnames, before any HTML redirection", async () => {
+		const metadata = createMetadataObjectWithRedirects([
+			{ from: "/", to: "/home", status: 301 },
+			{ from: "/page.html", to: "/elsewhere", status: 301 },
+		]);
+		const findAssetEntryForPath = async (path: string) => {
+			if (path === "/index.html") {
+				return "index page";
+			}
+
+			if (path === "/page.html") {
+				return "some page";
+			}
+
+			return null;
+		};
+
+		{
+			const { response } = await getTestResponse({
+				request: "https://example.com/index.html",
+				metadata,
+				findAssetEntryForPath,
+			});
+			expect(response.status).toBe(308);
+			expect(response.headers.get("Location")).toEqual("/");
+		}
+		{
+			const { response } = await getTestResponse({
+				request: "https://example.com/index",
+				metadata,
+				findAssetEntryForPath,
+			});
+			expect(response.status).toBe(308);
+			expect(response.headers.get("Location")).toEqual("/");
+		}
+		{
+			const { response } = await getTestResponse({
+				request: "https://example.com/",
+				metadata,
+				findAssetEntryForPath,
+			});
+			expect(response.status).toBe(301);
+			expect(response.headers.get("Location")).toEqual("/home");
+		}
+		{
+			// The redirect rule takes precedence to the 308s
+			const { response } = await getTestResponse({
+				request: "https://example.com/page.html",
+				metadata,
+				findAssetEntryForPath,
+			});
+			expect(response.status).toBe(301);
+			expect(response.headers.get("Location")).toEqual("/elsewhere");
+		}
+		{
+			// This serves the HTML even though the redirect rule is present
+			const { response, spies } = await getTestResponse({
+				request: "https://example.com/page",
+				metadata,
+				findAssetEntryForPath,
+			});
+			expect(response.status).toBe(200);
+			expect(spies.fetchAsset).toBe(1);
+		}
+	});
+
+	test("cross-host static redirects still are executed with line number precedence", async () => {
+		const metadata = createMetadataObjectWithRedirects([
+			{ from: "https://fakehost/home", to: "https://firsthost/", status: 302 },
+			{ from: "/home", to: "https://secondhost/", status: 302 },
+		]);
+		const findAssetEntryForPath = async (path: string) => {
+			if (path === "/index.html") {
+				return "index page";
+			}
+
+			return null;
+		};
+
+		{
+			const { response } = await getTestResponse({
+				request: "https://yetanotherhost/home",
+				metadata,
+				findAssetEntryForPath,
+			});
+			expect(response.status).toBe(302);
+			expect(response.headers.get("Location")).toEqual("https://secondhost/");
+		}
+		{
+			const { response } = await getTestResponse({
+				request: "https://fakehost/home",
+				metadata,
+				findAssetEntryForPath,
+			});
+			expect(response.status).toBe(302);
+			expect(response.headers.get("Location")).toEqual("https://firsthost/");
+		}
+	});
+
+	test("it should preserve querystrings unless to rule includes them", async () => {
+		const metadata = createMetadataObjectWithRedirects([
+			{ from: "/", status: 301, to: "/home" },
+			{ from: "/recent", status: 301, to: "/home?sort=updated_at" },
+		]);
+		const findAssetEntryForPath = async (path: string) => {
+			if (path === "/home.html") {
+				return "home page";
+			}
+
+			return null;
+		};
+
+		{
+			const { response } = await getTestResponse({
+				request: "https://example.com/?sort=price",
+				metadata,
+				findAssetEntryForPath,
+			});
+			expect(response.status).toBe(301);
+			expect(response.headers.get("Location")).toEqual("/home?sort=price");
+		}
+		{
+			const { response } = await getTestResponse({
+				request: "https://example.com/recent",
+				metadata,
+				findAssetEntryForPath,
+			});
+			expect(response.status).toBe(301);
+			expect(response.headers.get("Location")).toEqual("/home?sort=updated_at");
+		}
+		{
+			const { response } = await getTestResponse({
+				request: "https://example.com/recent?other=query",
+				metadata,
+				findAssetEntryForPath,
+			});
+			expect(response.status).toBe(301);
+			expect(response.headers.get("Location")).toEqual("/home?sort=updated_at");
+		}
+	});
+
+	{
+		const metadata = createMetadataObjectWithRedirects([
+			{ from: "/home", status: 301, to: "/" },
+			{ from: "/blog/*", status: 301, to: "https://blog.example.com/:splat" },
+			{
+				from: "/products/:code/:name/*",
+				status: 301,
+				to: "/products?junk=:splat&name=:name&code=:code",
+			},
+			{ from: "/foo", status: 301, to: "/bar" },
+		]);
+		const findAssetEntryForPath = async (path: string) => {
+			if (path === "/home.html") {
+				return "home page";
+			}
+
+			return null;
+		};
+
+		test("it should perform splat replacements", async () => {
+			const { response } = await getTestResponse({
+				request: "https://example.com/blog/a-blog-posting",
+				metadata,
+				findAssetEntryForPath,
+			});
+			expect(response.status).toBe(301);
+			expect(response.headers.get("Location")).toEqual(
+				"https://blog.example.com/a-blog-posting"
+			);
+		});
+
+		test("it should perform placeholder replacements", async () => {
+			const { response } = await getTestResponse({
+				request: "https://example.com/products/abba_562/tricycle/123abc@~!",
+				metadata,
+				findAssetEntryForPath,
+			});
+			expect(response.status).toBe(301);
+			expect(response.headers.get("Location")).toEqual(
+				"/products?junk=123abc@~!&name=tricycle&code=abba_562"
+			);
+		});
+
+		test("it should redirect both dynamic and static redirects", async () => {
+			{
+				const { response } = await getTestResponse({
+					request: "https://example.com/home",
+					metadata,
+					findAssetEntryForPath,
+				});
+				expect(response.status).toBe(301);
+				expect(response.headers.get("Location")).toEqual("/");
+			}
+			{
+				const { response } = await getTestResponse({
+					request: "https://example.com/blog/post",
+					metadata,
+					findAssetEntryForPath,
+				});
+				expect(response.status).toBe(301);
+				expect(response.headers.get("Location")).toEqual(
+					"https://blog.example.com/post"
+				);
+			}
+			{
+				const { response } = await getTestResponse({
+					request: "https://example.com/foo",
+					metadata,
+					findAssetEntryForPath,
+				});
+				expect(response.status).toBe(301);
+				expect(response.headers.get("Location")).toEqual("/bar");
+			}
+		});
+	}
+
+	test("Returns a redirect without duplicating the hash component", async () => {
+		const { response, spies } = await getTestResponse({
+			request: "https://foo.com/bar",
+			metadata: createMetadataObjectWithRedirects([
+				{ from: "/bar", to: "https://foobar.com/##heading-7", status: 301 },
+			]),
+		});
+
+		expect(spies.fetchAsset).toBe(0);
+		expect(spies.findAssetEntryForPath).toBe(0);
+		expect(spies.getAssetKey).toBe(0);
+		expect(spies.negotiateContent).toBe(0);
+		expect(response.status).toBe(301);
+		expect(response.headers.get("Location")).toBe(
+			"https://foobar.com/##heading-7"
+		);
+	});
+
+	test("it should redirect uri-encoded paths", async () => {
+		const { response, spies } = await getTestResponse({
+			request: "https://foo.com/some%20page",
+			metadata: createMetadataObjectWithRedirects([
+				{ from: "/some%20page", to: "/home", status: 301 },
+			]),
+		});
+
+		expect(spies.fetchAsset).toBe(0);
+		expect(spies.findAssetEntryForPath).toBe(0);
+		expect(spies.getAssetKey).toBe(0);
+		expect(spies.negotiateContent).toBe(0);
+		expect(response.status).toBe(301);
+		expect(response.headers.get("Location")).toBe("/home");
+	});
+
+	test("getResponseFromMatch - same origin paths specified as root-relative", () => {
+		const res = getResponseFromMatch(
+			{
+				to: "/bar",
+				status: 301,
+			},
+			new URL("https://example.com/foo")
+		);
+
+		expect(res.status).toBe(301);
+		expect(res.headers.get("Location")).toBe("/bar");
+	});
+
+	test("getResponseFromMatch - same origin paths specified as full URLs", () => {
+		const res = getResponseFromMatch(
+			{
+				to: "https://example.com/bar",
+				status: 301,
+			},
+			new URL("https://example.com/foo")
+		);
+
+		expect(res.status).toBe(301);
+		expect(res.headers.get("Location")).toBe("/bar");
+	});
+
+	test("early hints should cache link headers", async () => {
+		const deploymentId = "deployment-" + Math.random();
+		const metadata = createMetadataObject({ deploymentId }) as Metadata;
+
+		const findAssetEntryForPath = async (path: string) => {
+			if (path === "/index.html") {
+				return "index.html";
+			}
+
+			return null;
+		};
+
+		const { response, spies } = await getTestResponse({
+			request: new Request("https://example.com/"),
+			metadata,
+			findAssetEntryForPath,
+			fetchAsset: () =>
+				Promise.resolve(
+					Object.assign(
+						new Response(`
+							<!DOCTYPE html>
+							<html>
+								<body>
+									<link rel="preload" as="image" href="/a.png" />
+									<link rel="preload" as="image" href="/b.png" />
+								</body>
+							</html>`),
+						{ contentType: "text/html" }
+					)
+				),
+		});
+
+		expect(response.status).toBe(200);
+		// waitUntil should be called twice: once for asset-preservation, once for early hints
+		expect(spies.waitUntil.length).toBe(2);
+
+		await Promise.all(spies.waitUntil);
+
+		const earlyHintsCache = spies.caches[`eh:${deploymentId}`];
+		const earlyHintsRes = await earlyHintsCache.match("https://example.com/");
+
+		if (!earlyHintsRes) {
+			throw new Error(
+				"Did not match early hints cache on https://example.com/"
+			);
+		}
+
+		expect(earlyHintsRes.headers.get("link")).toMatchInlineSnapshot(
+			`"</a.png>; rel=\\"preload\\"; as=image, </b.png>; rel=\\"preload\\"; as=image"`
+		);
+	});
+
+	test("early hints should not cache link headers on non-html", async () => {
+		const deploymentId = "deployment-" + Math.random();
+		const metadata = createMetadataObject({ deploymentId }) as Metadata;
+
+		const findAssetEntryForPath = async (path: string) => {
+			if (path === "/index.js") {
+				return "index.js";
+			}
+
+			return null;
+		};
+
+		const { response, spies } = await getTestResponse({
+			request: new Request("https://example.com/index.js"),
+			metadata,
+			findAssetEntryForPath,
+			fetchAsset: () =>
+				Promise.resolve(
+					Object.assign(
+						new Response(`
+							<!DOCTYPE html>
+							<html>
+								<body>
+									<link rel="preload" as="image" href="/a.png" />
+									<link rel="preload" as="image" href="/b.png" />
+								</body>
+							</html>`),
+						{ contentType: "application/javascript" }
+					)
+				),
+		});
+
+		expect(response.status).toBe(200);
+		// waitUntil should be called once for asset-preservation
+		expect(spies.waitUntil.length).toBe(1);
+
+		await Promise.all(spies.waitUntil);
+
+		const earlyHintsCache = spies.caches[`eh:${deploymentId}`];
+		const earlyHintsRes = await earlyHintsCache.match(
+			"https://example.com/index.js"
+		);
+
+		expect(earlyHintsRes).toBeUndefined();
+	});
+});
+
+test("getResponseFromMatch - different origins", () => {
+	const res = getResponseFromMatch(
+		{
+			to: "https://bar.com/bar",
+			status: 302,
+		},
+		new URL("https://example.com/foo")
+	);
+
+	expect(res.status).toBe(302);
+	expect(res.headers.get("Location")).toBe("https://bar.com/bar");
+});
+
+interface HandlerSpies {
+	fetchAsset: number;
+	findAssetEntryForPath: number;
+	getAssetKey: number;
+	negotiateContent: number;
+	waitUntil: Promise<unknown>[];
+	caches: {
+		[key: string]: Cache;
+	} & { default: Cache };
+}
+
+async function getTestResponse({
+	request,
+	metadata = createMetadataObject({
+		deploymentId: "mock-deployment-id",
+		redirects: {
+			invalid: [],
+			rules: [],
+		},
+	}) as Metadata,
+	...options
+}: {
+	request: Request | string;
+} & Omit<Partial<HandlerContext<string>>, "request">): Promise<{
+	response: Response;
+	spies: HandlerSpies;
+}> {
+	const spies: HandlerSpies = {
+		fetchAsset: 0,
+		findAssetEntryForPath: 0,
+		getAssetKey: 0,
+		negotiateContent: 0,
+		waitUntil: [],
+		caches: {
+			default: new Cache(new MemoryStorage()),
+		},
+	};
+
+	const response = await generateHandler<string>({
+		request: request instanceof Request ? request : new Request(request),
+		metadata,
+		xServerEnvHeader: "dev",
+		logError: console.error,
+		findAssetEntryForPath: async (...args) => {
+			spies.findAssetEntryForPath++;
+			return options.findAssetEntryForPath?.(...args) ?? null;
+		},
+		getAssetKey: (assetEntry, content) => {
+			spies.getAssetKey++;
+			return options.getAssetKey?.(assetEntry, content) ?? assetEntry;
+		},
+		negotiateContent: (...args) => {
+			spies.negotiateContent++;
+			return options.negotiateContent?.(...args) ?? { encoding: null };
+		},
+		fetchAsset: async (...args) => {
+			spies.fetchAsset++;
+			return (
+				options.fetchAsset?.(...args) ?? {
+					body: null,
+					contentType: "text/plain",
+				}
+			);
+		},
+		waitUntil: async (promise: Promise<unknown>) => {
+			spies.waitUntil.push(promise);
+		},
+		caches: {
+			...spies.caches,
+			open(cacheName) {
+				const cache = new Cache(new MemoryStorage());
+				spies.caches[cacheName] = cache;
+				return Promise.resolve(cache);
+			},
+		},
+	});
+
+	return { response, spies };
+}
+
+function createMetadataObjectWithRedirects(
+	rules: Pick<RedirectRule, "from" | "to" | "status">[]
+): Metadata {
+	return createMetadataObject({
+		redirects: {
+			invalid: [],
+			rules: rules.map((rule, i) => ({ ...rule, lineNumber: i + 1 })),
+		},
+	}) as Metadata;
+}
---
+++ packages/wrangler/src/d1/execute.tsx
@@ -0,0 +1,321 @@
+import { existsSync } from "node:fs";
+import { mkdir } from "node:fs/promises";
+import path from "node:path";
+import chalk from "chalk";
+import { render, Static, Text } from "ink";
+import Table from "ink-table";
+import { npxImport } from "npx-import";
+import React from "react";
+import { fetchResult } from "../cfetch";
+import { readConfig } from "../config";
+import { getLocalPersistencePath } from "../dev/get-local-persistence-path";
+import { confirm } from "../dialogs";
+import { logger } from "../logger";
+import { readFileSync } from "../parse";
+import { readableRelative } from "../paths";
+import { requireAuth } from "../user";
+import * as options from "./options";
+import splitSqlQuery from "./splitter";
+import {
+	d1BetaWarning,
+	getDatabaseByNameOrBinding,
+	getDatabaseInfoFromConfig,
+} from "./utils";
+import type { Config, ConfigFields, DevConfig, Environment } from "../config";
+import type {
+	CommonYargsArgv,
+	StrictYargsOptionsToInterface,
+} from "../yargs-types";
+import type { Database } from "./types";
+import type { Statement as StatementType } from "@miniflare/d1";
+import type { createSQLiteDB as createSQLiteDBType } from "@miniflare/shared";
+
+type MiniflareNpxImportTypes = [
+	{
+		Statement: typeof StatementType;
+	},
+	{
+		createSQLiteDB: typeof createSQLiteDBType;
+	}
+];
+
+export type QueryResult = {
+	results: Record<string, string | number | boolean>[];
+	success: boolean;
+	meta?: {
+		duration?: number;
+	};
+	query?: string;
+};
+// Max number of bytes to send in a single /execute call
+const QUERY_LIMIT = 10_000;
+
+export function Options(yargs: CommonYargsArgv) {
+	return options
+		.Database(yargs)
+		.option("yes", {
+			describe: 'Answer "yes" to any prompts',
+			type: "boolean",
+			alias: "y",
+		})
+		.option("local", {
+			describe:
+				"Execute commands/files against a local DB for use with wrangler dev",
+			type: "boolean",
+		})
+		.option("file", {
+			describe: "A .sql file to injest",
+			type: "string",
+		})
+		.option("command", {
+			describe: "A single SQL statement to execute",
+			type: "string",
+		})
+		.option("persist-to", {
+			describe: "Specify directory to use for local persistence (for --local)",
+			type: "string",
+			requiresArg: true,
+		})
+		.option("json", {
+			describe: "Return output as clean JSON",
+			type: "boolean",
+			default: false,
+		});
+}
+
+function shorten(query: string | undefined, length: number) {
+	return query && query.length > length
+		? query.slice(0, length) + "..."
+		: query;
+}
+
+export async function executeSql(
+	local: undefined | boolean,
+	config: ConfigFields<DevConfig> & Environment,
+	name: string,
+	shouldPrompt: boolean | undefined,
+	persistTo: undefined | string,
+	file?: string,
+	command?: string,
+	json?: boolean
+) {
+	const sql = file ? readFileSync(file) : command;
+	if (!sql) throw new Error(`Error: must provide --command or --file.`);
+	if (persistTo && !local)
+		throw new Error(`Error: can't use --persist-to without --local`);
+	logger.log(`🌀 Mapping SQL input into an array of statements`);
+	const queries = splitSqlQuery(sql);
+
+	if (file && sql) {
+		if (queries[0].startsWith("SQLite format 3")) {
+			//TODO: update this error to recommend using `wrangler d1 restore` when it exists
+			throw new Error(
+				"Provided file is a binary SQLite database file instead of an SQL text file.\nThe execute command can only process SQL text files.\nPlease export an SQL file from your SQLite database and try again."
+			);
+		}
+	}
+
+	return local
+		? await executeLocally(config, name, shouldPrompt, queries, persistTo, json)
+		: await executeRemotely(
+				config,
+				name,
+				shouldPrompt,
+				batchSplit(queries),
+				json
+		  );
+}
+type HandlerOptions = StrictYargsOptionsToInterface<typeof Options>;
+
+export const Handler = async (args: HandlerOptions): Promise<void> => {
+	const { local, database, yes, persistTo, file, command, json } = args;
+	const existingLogLevel = logger.loggerLevel;
+	if (json) {
+		// set loggerLevel to error to avoid readConfig warnings appearing in JSON output
+		logger.loggerLevel = "error";
+	}
+	const config = readConfig(args.config, args);
+	logger.log(d1BetaWarning);
+	if (file && command)
+		return logger.error(`Error: can't provide both --command and --file.`);
+
+	const isInteractive = process.stdout.isTTY;
+	const response: QueryResult[] | null = await executeSql(
+		local,
+		config,
+		database,
+		isInteractive && !yes,
+		persistTo,
+		file,
+		command,
+		json
+	);
+
+	// Early exit if prompt rejected
+	if (!response) return;
+
+	if (isInteractive && !json) {
+		// Render table if single result
+		render(
+			<Static items={response}>
+				{(result) => {
+					// batch results
+					if (!Array.isArray(result)) {
+						const { results, query } = result;
+
+						if (Array.isArray(results) && results.length > 0) {
+							const shortQuery = shorten(query, 48);
+							return (
+								<>
+									{shortQuery ? <Text dimColor>{shortQuery}</Text> : null}
+									<Table data={results}></Table>
+								</>
+							);
+						}
+					}
+				}}
+			</Static>
+		);
+	} else {
+		// set loggerLevel back to what it was before to actually output the JSON in stdout
+		logger.loggerLevel = existingLogLevel;
+		logger.log(JSON.stringify(response, null, 2));
+	}
+};
+
+async function executeLocally(
+	config: Config,
+	name: string,
+	shouldPrompt: boolean | undefined,
+	queries: string[],
+	persistTo: string | undefined,
+	json?: boolean
+) {
+	const localDB = getDatabaseInfoFromConfig(config, name);
+	if (!localDB) {
+		throw new Error(
+			`Can't find a DB with name/binding '${name}' in local config. Check info in wrangler.toml...`
+		);
+	}
+
+	const persistencePath = getLocalPersistencePath(
+		persistTo,
+		true,
+		config.configPath
+	);
+
+	const dbDir = path.join(persistencePath, "d1");
+	const dbPath = path.join(dbDir, `${localDB.binding}.sqlite3`);
+	const [{ Statement }, { createSQLiteDB }] =
+		await npxImport<MiniflareNpxImportTypes>(
+			["@miniflare/d1", "@miniflare/shared"],
+			logger.log
+		);
+
+	if (!existsSync(dbDir)) {
+		const ok =
+			json ||
+			!shouldPrompt ||
+			(await confirm(`About to create ${readableRelative(dbPath)}, ok?`));
+		if (!ok) return null;
+		await mkdir(dbDir, { recursive: true });
+	}
+
+	logger.log(`🌀 Loading DB at ${readableRelative(dbPath)}`);
+	const db = await createSQLiteDB(dbPath);
+
+	const results: QueryResult[] = [];
+	for (const sql of queries) {
+		const statement = new Statement(db, sql);
+		results.push((await statement.all()) as QueryResult);
+	}
+
+	return results;
+}
+
+async function executeRemotely(
+	config: Config,
+	name: string,
+	shouldPrompt: boolean | undefined,
+	batches: string[],
+	json?: boolean
+) {
+	const multiple_batches = batches.length > 1;
+	// in JSON mode, we don't want a prompt here
+	if (multiple_batches && !json) {
+		const warning = `⚠️  Too much SQL to send at once, this execution will be sent as ${batches.length} batches.`;
+
+		if (shouldPrompt) {
+			const ok = await confirm(
+				`${warning}\nℹ️  Each batch is sent individually and may leave your DB in an unexpected state if a later batch fails.\n⚠️  Make sure you have a recent backup. Ok to proceed?`
+			);
+			if (!ok) return null;
+			logger.log(`🌀 Let's go`);
+		} else {
+			logger.error(warning);
+		}
+	}
+
+	const accountId = await requireAuth({});
+	const db: Database = await getDatabaseByNameOrBinding(
+		config,
+		accountId,
+		name
+	);
+
+	logger.log(`🌀 Executing on ${name} (${db.uuid}):`);
+
+	const results: QueryResult[] = [];
+	for (const sql of batches) {
+		if (multiple_batches)
+			logger.log(
+				chalk.gray(`  ${sql.slice(0, 70)}${sql.length > 70 ? "..." : ""}`)
+			);
+
+		const result = await fetchResult<QueryResult[]>(
+			`/accounts/${accountId}/d1/database/${db.uuid}/query`,
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					...(db.internal_env ? { "x-d1-internal-env": db.internal_env } : {}),
+				},
+				body: JSON.stringify({ sql }),
+			}
+		);
+		result.map(logResult);
+		results.push(...result);
+	}
+	return results;
+}
+
+function logResult(r: QueryResult | QueryResult[]) {
+	logger.log(
+		`🚣 Executed ${
+			Array.isArray(r) ? `${r.length} commands` : "1 command"
+		} in ${
+			Array.isArray(r)
+				? r
+						.map((d: QueryResult) => d.meta?.duration || 0)
+						.reduce((a: number, b: number) => a + b, 0)
+				: r.meta?.duration
+		}ms`
+	);
+}
+
+function batchSplit(queries: string[]) {
+	logger.log(`🌀 Parsing ${queries.length} statements`);
+	const num_batches = Math.ceil(queries.length / QUERY_LIMIT);
+	const batches: string[] = [];
+	for (let i = 0; i < num_batches; i++) {
+		batches.push(
+			queries.slice(i * QUERY_LIMIT, (i + 1) * QUERY_LIMIT).join("; ")
+		);
+	}
+	if (num_batches > 1) {
+		logger.log(
+			`🌀 We are sending ${num_batches} batch(es) to D1 (limited to ${QUERY_LIMIT} statements per batch)`
+		);
+	}
+	return batches;
+}
