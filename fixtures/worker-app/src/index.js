b4a13e34bf91a63ad5a70f5205399007f4b5dffc
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:41:27 +0300

    Initialize gv/cli-pr

---
+++ fixtures/worker-app/src/index.js
@@ -0,0 +1,63 @@
+import { randomBytes } from "isomorphic-random-example";
+import { now } from "./dep";
+import { logErrors } from "./log";
+
+console.log("startup log");
+
+/** @param {Uint8Array} array */
+function hexEncode(array) {
+	return Array.from(array)
+		.map((x) => x.toString(16).padStart(2, "0"))
+		.join("");
+}
+
+export default {
+	async fetch(request) {
+		console.log("request log");
+
+		const { pathname } = new URL(request.url);
+		if (pathname === "/random") return new Response(hexEncode(randomBytes(8)));
+		if (pathname === "/error") throw new Error("Oops!");
+
+		console.log(
+			request.method,
+			request.url,
+			new Map([...request.headers]),
+			request.cf
+		);
+
+		logErrors();
+
+		await fetch(new URL("http://example.com"));
+		await fetch(
+			new Request("http://example.com", { method: "POST", body: "foo" })
+		);
+
+		console.log("end of request");
+		return new Response(
+			`${request.url} ${now()} ${request.headers.get("Host")}`
+		);
+	},
+
+	/**
+	 * Handle a scheduled event.
+	 *
+	 * If developing using `--local` mode, you can trigger this scheduled event via a CURL.
+	 * E.g. `curl "http://localhost:8787/cdn-cgi/mf/scheduled"`.
+	 * See the Miniflare docs: https://miniflare.dev/core/scheduled.
+	 */
+	scheduled(event, env, ctx) {
+		ctx.waitUntil(Promise.resolve(event.scheduledTime));
+		ctx.waitUntil(Promise.resolve(event.cron));
+	},
+};
+
+// addEventListener("fetch", (event) => {
+//   event.respondWith(handleRequest(event.request));
+// });
+
+// async function handleRequest(request) {
+//   return new Response("Hello worker!", {
+//     headers: { "content-type": "text/plain" },
+//   });
+// }
---
+++ fixtures/worker-app/tests/index.test.ts
@@ -0,0 +1,79 @@
+import { resolve } from "path";
+import { setTimeout } from "timers/promises";
+import { fetch } from "undici";
+import { describe, it, beforeAll, afterAll } from "vitest";
+import { runWranglerDev } from "../../shared/src/run-wrangler-long-lived";
+
+describe("'wrangler dev' correctly renders pages", () => {
+	let ip: string,
+		port: number,
+		stop: (() => Promise<unknown>) | undefined,
+		getOutput: () => string;
+
+	beforeAll(async () => {
+		({ ip, port, stop, getOutput } = await runWranglerDev(
+			resolve(__dirname, ".."),
+			["--port=0", "--inspector-port=0"]
+		));
+	});
+
+	afterAll(async () => {
+		await stop?.();
+	});
+
+	it("renders ", async ({ expect }) => {
+		const response = await fetch(`http://${ip}:${port}/`);
+		const text = await response.text();
+		expect(response.status).toBe(200);
+		expect(text).toContain(`http://${ip}:${port}/`);
+
+		// Wait up to 5s for all request logs to be flushed
+		for (let i = 0; i < 10; i++) {
+			if (getOutput().includes("end of request")) break;
+			await setTimeout(500);
+		}
+
+		// Ensure `console.log()`s from startup and requests are shown
+		const output = getOutput();
+		expect(output).toContain("startup log");
+		expect(output).toContain("request log");
+
+		// check host on request in the Worker is as expected
+		expect(output).toContain(`host' => '${ip}:${port}'`);
+
+		// Check logged strings are source mapped
+		expect(output).toMatch(
+			/Error: logged error one\n.+at logErrors.+fixtures\/worker-app\/src\/log\.ts:7:14/
+		);
+		expect(output).toMatch(
+			/Error: logged error two\n.+at logErrors.+fixtures\/worker-app\/src\/log\.ts:8:14/
+		);
+		expect(output).toMatch(
+			/Error: logged error three\n.+at logErrors.+fixtures\/worker-app\/src\/log\.ts:9:23/
+		);
+		expect(output).toMatch(
+			/Error: logged error four\\n' \+\n.+at logErrors.+fixtures\/worker-app\/src\/log\.ts:10:33/
+		);
+
+		// Regression test for https://github.com/cloudflare/workers-sdk/issues/4668
+		expect(output).toContain("some normal text to log");
+		expect(output).toContain("text with at in the middle");
+		expect(output).toContain("more text with    at in the middle");
+	});
+
+	it("renders pretty error after logging", async ({ expect }) => {
+		// Regression test for https://github.com/cloudflare/workers-sdk/issues/4715
+		const response = await fetch(`http://${ip}:${port}/error`);
+		const text = await response.text();
+		expect(text).toContain("Oops!");
+		expect(response.headers.get("Content-Type")).toBe(
+			"text/html;charset=utf-8"
+		);
+	});
+
+	it("uses `workerd` condition when bundling", async ({ expect }) => {
+		const response = await fetch(`http://${ip}:${port}/random`);
+		const text = await response.text();
+		expect(text).toMatch(/[0-9a-f]{16}/); // 8 hex bytes
+	});
+});
