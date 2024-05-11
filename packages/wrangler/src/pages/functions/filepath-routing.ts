7e71a4eb792ab50957eec424a087ecd62ed3ea7c
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:31:01 +0300

    Initialize rfc-export-default-fetch-pages-functions

---
+++ packages/wrangler/src/pages/functions/filepath-routing.ts
@@ -0,0 +1,237 @@
+import fs from "node:fs/promises";
+import { tmpdir } from "node:os";
+import path from "node:path";
+import { build } from "esbuild";
+import { toUrlPath } from "../../paths";
+import { loader } from "./esbuild";
+import type { UrlPath } from "../../paths";
+import type { HTTPMethod, RouteConfig } from "./routes";
+import { logger } from "../../logger";
+
+export async function generateConfigFromFileTree({
+	baseDir,
+	baseURL,
+}: {
+	baseDir: string;
+	baseURL: UrlPath;
+}) {
+	let routeEntries: RouteConfig[] = [];
+
+	if (!baseURL.startsWith("/")) {
+		baseURL = `/${baseURL}` as UrlPath;
+	}
+
+	if (baseURL.endsWith("/")) {
+		baseURL = baseURL.slice(0, -1) as UrlPath;
+	}
+
+	await forEachFile(baseDir, async (filepath) => {
+		const ext = path.extname(filepath);
+		const outfile = path.join(
+			tmpdir(),
+			`${Math.random().toString(36).slice(2, 8)}.mjs`
+		);
+		if (/^\.(mjs|js|ts|tsx|jsx)$/.test(ext)) {
+			const { metafile } = await build({
+				metafile: true,
+				outfile,
+				format: "esm",
+				target: "es6",
+				loader,
+				bundle: true,
+				entryPoints: [path.resolve(filepath)],
+				plugins: [
+					{
+						name: "Assets",
+						setup(pluginBuild) {
+							pluginBuild.onResolve({ filter: /^assets:/ }, () => {
+								return { path: filepath, namespace: "assets" };
+							});
+
+							pluginBuild.onLoad({ filter: /.*/, namespace: "assets" }, () => {
+								return {
+									contents: `export const onRequest = () => {
+											return new Response("Static asset")
+										}`,
+								};
+							});
+						},
+					},
+				],
+			});
+			const exportNames: string[] = [];
+			if (metafile) {
+				for (const output in metafile?.outputs) {
+					exportNames.push(...metafile.outputs[output].exports);
+				}
+			}
+			for (const exportName of exportNames) {
+				const [match, method = ""] = (exportName.match(
+					/^onRequest(Get|Post|Put|Patch|Delete|Options|Head)?|default?$/
+				) ?? []) as (string | undefined)[];
+
+				if (match) {
+					const basename = path.basename(filepath).slice(0, -ext.length);
+
+					if (match === "default") {
+						try {
+							if (!("fetch" in (await import(outfile)).default)) {
+								continue;
+							}
+						} catch {}
+					}
+
+					const isIndexFile = basename === "index";
+					// TODO: deprecate _middleware_ in favor of _middleware
+					const isMiddlewareFile =
+						basename === "_middleware" || basename === "_middleware_";
+
+					let routePath = path
+						.relative(baseDir, filepath)
+						.slice(0, -ext.length);
+					let mountPath = path.dirname(routePath);
+
+					if (isIndexFile || isMiddlewareFile) {
+						routePath = path.dirname(routePath);
+					}
+
+					if (routePath === ".") {
+						routePath = "";
+					}
+					if (mountPath === ".") {
+						mountPath = "";
+					}
+
+					routePath = `${baseURL}/${routePath}`;
+					mountPath = `${baseURL}/${mountPath}`;
+
+					routePath = routePath.replace(/\[\[([^\]]+)\]\]/g, ":$1*"); // transform [[id]] => :id*
+					routePath = routePath.replaceAll(/\[([^\]]+)\]/g, ":$1"); // transform [id] => :id
+					mountPath = mountPath.replace(/\[\[([^\]]+)\]\]/g, ":$1*"); // transform [[id]] => :id*
+					mountPath = mountPath.replaceAll(/\[([^\]]+)\]/g, ":$1"); // transform [id] => :id
+
+					// These are used as module specifiers so UrlPaths are okay to use even on Windows
+					const modulePath = toUrlPath(path.relative(baseDir, filepath));
+
+					const routeEntry: RouteConfig = {
+						routePath: toUrlPath(routePath),
+						mountPath: toUrlPath(mountPath),
+						method: method.toUpperCase() as HTTPMethod,
+						[isMiddlewareFile ? "middleware" : "module"]: [
+							`${modulePath}:${exportName}`,
+						],
+					};
+
+					routeEntries.push(routeEntry);
+				}
+			}
+		}
+	});
+	// Combine together any routes (index routes) which contain both a module and a middleware
+	routeEntries = routeEntries.reduce(
+		(acc: typeof routeEntries, { routePath, ...rest }) => {
+			const existingRouteEntry = acc.find(
+				(routeEntry) =>
+					routeEntry.routePath === routePath &&
+					routeEntry.method === rest.method
+			);
+			if (existingRouteEntry !== undefined) {
+				if (rest.module && existingRouteEntry.module) {
+					logger.warn(
+						`Caution: duplicate Functions handler detected. You specify two different handlers with the same route (${
+							existingRouteEntry.routePath
+						}) ${
+							existingRouteEntry.method
+								? `and method (${existingRouteEntry.method})`
+								: ""
+						}. This may lead to unexpected results. Remove one of these handlers to prevent this warning.`
+					);
+				}
+
+				Object.assign(existingRouteEntry, rest);
+			} else {
+				acc.push({ routePath, ...rest });
+			}
+			return acc;
+		},
+		[]
+	);
+
+	routeEntries.sort((a, b) => compareRoutes(a, b));
+
+	return {
+		routes: routeEntries,
+	};
+}
+
+// Ensure routes are produced in order of precedence so that
+// more specific routes aren't occluded from matching due to
+// less specific routes appearing first in the route list.
+export function compareRoutes(
+	{ routePath: routePathA, method: methodA }: RouteConfig,
+	{ routePath: routePathB, method: methodB }: RouteConfig
+) {
+	function parseRoutePath(routePath: UrlPath): string[] {
+		return routePath.slice(1).split("/").filter(Boolean);
+	}
+
+	const segmentsA = parseRoutePath(routePathA);
+	const segmentsB = parseRoutePath(routePathB);
+
+	// sort routes with fewer segments after those with more segments
+	if (segmentsA.length !== segmentsB.length) {
+		return segmentsB.length - segmentsA.length;
+	}
+
+	for (let i = 0; i < segmentsA.length; i++) {
+		const isWildcardA = segmentsA[i].includes("*");
+		const isWildcardB = segmentsB[i].includes("*");
+		const isParamA = segmentsA[i].includes(":");
+		const isParamB = segmentsB[i].includes(":");
+
+		// sort wildcard segments after non-wildcard segments
+		if (isWildcardA && !isWildcardB) return 1;
+		if (!isWildcardA && isWildcardB) return -1;
+
+		// sort dynamic param segments after non-param segments
+		if (isParamA && !isParamB) return 1;
+		if (!isParamA && isParamB) return -1;
+	}
+
+	// sort routes that specify an HTTP before those that don't
+	if (methodA && !methodB) return -1;
+	if (!methodA && methodB) return 1;
+
+	// all else equal, just sort the paths lexicographically
+	return routePathA.localeCompare(routePathB);
+}
+
+async function forEachFile<T>(
+	baseDir: string,
+	fn: (filepath: string) => T | Promise<T>
+) {
+	const searchPaths = [baseDir];
+	const returnValues: T[] = [];
+
+	while (isNotEmpty(searchPaths)) {
+		const cwd = searchPaths.shift();
+		const dir = await fs.readdir(cwd, { withFileTypes: true });
+		for (const entry of dir) {
+			const pathname = path.join(cwd, entry.name);
+			if (entry.isDirectory()) {
+				searchPaths.push(pathname);
+			} else if (entry.isFile()) {
+				returnValues.push(await fn(pathname));
+			}
+		}
+	}
+
+	return returnValues;
+}
+
+interface NonEmptyArray<T> extends Array<T> {
+	shift(): T;
+}
+function isNotEmpty<T>(array: T[]): array is NonEmptyArray<T> {
+	return array.length > 0;
+}
---
+++ packages/wranglerjs-compat-webpack-plugin/CHANGELOG.md
@@ -0,0 +1,33 @@
+# wranglerjs-compat-webpack-plugin
+
+## 0.0.5
+
+### Patch Changes
+
+- [`8529678`](https://github.com/cloudflare/wrangler2/commit/85296787adb1835054510a5df23a30ee08758971) Thanks [@threepointone](https://github.com/threepointone)! - fix: build the webpack plugin along with the others
+
+## 0.0.4
+
+### Patch Changes
+
+- [`70c01a2`](https://github.com/cloudflare/wrangler2/commit/70c01a2a13b1950be07d9b02cb3f12cbc91036ad) Thanks [@threepointone](https://github.com/threepointone)! - fix: include all files when publishing wranglerjs-compat-webpack-plugin
+
+## 0.0.3
+
+### Patch Changes
+
+- [`cf300ee`](https://github.com/cloudflare/wrangler2/commit/cf300eef4c6ca94386ed3cbcf19d470aa6972aca) Thanks [@threepointone](https://github.com/threepointone)! - fix: include files to be published with wranglerjs-compat-webpack-plugin
+
+## 0.0.2
+
+### Patch Changes
+
+- [#1034](https://github.com/cloudflare/wrangler2/pull/1034) [`53033f3`](https://github.com/cloudflare/wrangler2/commit/53033f3091e2d8fc675a0b078b36b3aa37673cba) Thanks [@threepointone](https://github.com/threepointone)! - Bumping wranglerjs-compat-webpack-plugin to do a release
+
+## 0.0.1
+
+### Patch Changes
+
+- [#759](https://github.com/cloudflare/wrangler2/pull/759) [`698f784`](https://github.com/cloudflare/wrangler2/commit/698f784ec33c574f374144c08638f21718db97a1) Thanks [@caass](https://github.com/caass)! - Create WranglerJsCompatWebpackPlugin
+
+  A webpack@4 plugin to emulate the behavior of wrangler 1's `type=webpack` for users that cannot switch to esbuild and are not already using a custom build.
