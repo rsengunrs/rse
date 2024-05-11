7515592bea3b320068a8d59ac8548570cd0969ea
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:45:39 +0300

    Initialize bib/python-templates

---
+++ packages/pages-shared/metadata-generator/types.ts
@@ -0,0 +1,96 @@
+/* REDIRECT PARSING TYPES */
+
+export type RedirectLine = [from: string, to: string, status?: number];
+export type RedirectRule = {
+	from: string;
+	to: string;
+	status: number;
+	lineNumber: number;
+};
+
+export type Headers = Record<string, string>;
+export type HeadersRule = {
+	path: string;
+	headers: Headers;
+	unsetHeaders: string[];
+};
+
+export type InvalidRedirectRule = {
+	line?: string;
+	lineNumber?: number;
+	message: string;
+};
+
+export type InvalidHeadersRule = {
+	line?: string;
+	lineNumber?: number;
+	message: string;
+};
+
+export type ParsedRedirects = {
+	invalid: InvalidRedirectRule[];
+	rules: RedirectRule[];
+};
+
+/** Parsed redirects and input file */
+export type ParsedRedirectsWithFile = {
+	parsedRedirects?: ParsedRedirects;
+	file?: string;
+};
+
+export type ParsedHeaders = {
+	invalid: InvalidHeadersRule[];
+	rules: HeadersRule[];
+};
+
+/** Parsed headers and input file */
+export type ParsedHeadersWithFile = {
+	parsedHeaders?: ParsedHeaders;
+	file?: string;
+};
+
+/* METADATA TYPES*/
+
+export type MetadataRedirectEntry = {
+	status: number;
+	to: string;
+};
+
+export type MetadataRedirects = {
+	[path: string]: MetadataRedirectEntry;
+};
+
+export type MetadataHeaders = {
+	[path: string]: MetadataHeaderEntry;
+};
+
+export type MetadataHeaderEntry = {
+	set?: Record<string, string>;
+	unset?: Array<string>;
+};
+
+export type Metadata = {
+	redirects?: {
+		version: number;
+		staticRules: MetadataRedirects;
+		rules: MetadataRedirects;
+	};
+	headers?: {
+		version: number;
+		rules: MetadataHeaders;
+	};
+	analytics?: {
+		version: number;
+		token: string;
+	};
+	deploymentId?: string;
+	failOpen?: boolean;
+};
+
+export interface Logger {
+	debug: (message: string) => void;
+	log: (message: string) => void;
+	info: (message: string) => void;
+	warn: (message: string) => void;
+	error: (error: Error) => void;
+}
