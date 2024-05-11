bb1a187478f69618a9323d20c0cc27c258db8413
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:34:18 +0300

    Initialize rozenmd/loc-hints

---
+++ packages/wrangler/src/d1/types.ts
@@ -0,0 +1,31 @@
+export type Database = {
+	uuid: string;
+	previewDatabaseUuid?: string;
+	name: string;
+	binding: string;
+	internal_env?: string;
+	migrationsTableName: string;
+	migrationsFolderPath: string;
+};
+
+export type DatabaseCreationResult = {
+	uuid: string;
+	name: string;
+	primary_location_hint?: string;
+};
+
+export type Backup = {
+	id: string;
+	database_id: string;
+	created_at: string;
+	state: "progress" | "done";
+	num_tables: number;
+	file_size: number;
+	size?: string;
+};
+
+export type Migration = {
+	id: string;
+	name: string;
+	applied_at: string;
+};
