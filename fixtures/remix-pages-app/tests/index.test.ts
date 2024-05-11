a2623ee805dae5d1d012ad9c5004b2d9ec8f6c63
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:36:53 +0300

    Initialize c3-e2e-concurrency-2

---
+++ fixtures/remix-pages-app/tests/index.test.ts
@@ -0,0 +1,33 @@
+import { spawnSync } from "node:child_process";
+import { resolve } from "node:path";
+import { fetch } from "undici";
+import { describe, it, beforeAll, afterAll } from "vitest";
+import { runWranglerPagesDev } from "../../shared/src/run-wrangler-long-lived";
+
+const isWindows = process.platform === "win32";
+
+describe("Remix", () => {
+	let ip: string;
+	let port: number;
+	let stop: () => void;
+
+	beforeAll(async () => {
+		spawnSync("npm", ["run", "build"], {
+			shell: isWindows,
+			cwd: resolve(__dirname, ".."),
+		});
+		({ ip, port, stop } = await runWranglerPagesDev(
+			resolve(__dirname, ".."),
+			"public",
+			["--port=0"]
+		));
+	});
+
+	afterAll(async () => await stop());
+
+	it("renders", async ({ expect }) => {
+		const response = await fetch(`http://${ip}:${port}/`);
+		const text = await response.text();
+		expect(text).toContain("Welcome to Remix");
+	});
+});
