e71dab06a0b700bebff184a21a46ca092f1fd114
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:48:15 +0300

    Initialize pr/webbertakken/5616

---
+++ fixtures/durable-objects-app/tests/index.test.ts
@@ -0,0 +1,26 @@
+import { execSync } from "node:child_process";
+import { resolve } from "node:path";
+import { assert, describe, it } from "vitest";
+
+describe("durable objects", () => {
+	it("should throw an error when the worker doesn't export a durable object but requires one", ({
+		expect,
+	}) => {
+		let err: string = "";
+		try {
+			execSync("pnpm run dev", {
+				cwd: resolve(__dirname, ".."),
+			});
+			assert(false); // Should never reach this
+		} catch (e) {
+			err = (e as Error).message
+				.replaceAll("✘", "X")
+				.replace(/\\/g, "/")
+				.replace(/[^\S\n]+\n/g, "\n")
+				.trimEnd();
+		}
+		expect(err).toContain(
+			"You should export these objects from your entrypoint, src/index.js."
+		);
+	});
+});
