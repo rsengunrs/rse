d6eab7b563a44d0d1455345af70f977b23666938
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:46:38 +0300

    Initialize dependabot/npm_and_yarn/packages/workers-playground/undici-5.28.4

---
+++ packages/vitest-pool-workers/CHANGELOG.md
@@ -0,0 +1,128 @@
+# @cloudflare/vitest-pool-workers
+
+## 0.1.15
+
+### Patch Changes
+
+- Updated dependencies [[`7734f80`](https://github.com/cloudflare/workers-sdk/commit/7734f806c1ac2a38faabc87df4aa8344b585c430)]:
+  - wrangler@3.47.0
+
+## 0.1.14
+
+### Patch Changes
+
+- Updated dependencies [[`cd03d1d`](https://github.com/cloudflare/workers-sdk/commit/cd03d1d3fa6e733faa42e5abb92f37637503b327), [`cd03d1d`](https://github.com/cloudflare/workers-sdk/commit/cd03d1d3fa6e733faa42e5abb92f37637503b327), [`b7ddde1`](https://github.com/cloudflare/workers-sdk/commit/b7ddde1a5165223dcbe8781e928039123778b8a1), [`cd03d1d`](https://github.com/cloudflare/workers-sdk/commit/cd03d1d3fa6e733faa42e5abb92f37637503b327), [`6c3be5b`](https://github.com/cloudflare/workers-sdk/commit/6c3be5b299b22cad050760a6015106839b5cc74e), [`cd03d1d`](https://github.com/cloudflare/workers-sdk/commit/cd03d1d3fa6e733faa42e5abb92f37637503b327), [`cd03d1d`](https://github.com/cloudflare/workers-sdk/commit/cd03d1d3fa6e733faa42e5abb92f37637503b327)]:
+  - wrangler@3.46.0
+  - miniflare@3.20240403.0
+
+## 0.1.13
+
+### Patch Changes
+
+- Updated dependencies [[`5d68744`](https://github.com/cloudflare/workers-sdk/commit/5d6874499049641c1d3d3f47161e7ebf3bc57650), [`68faf67`](https://github.com/cloudflare/workers-sdk/commit/68faf67f0499927d7bded1342ccc9c8c9e76037a), [`a232ccf`](https://github.com/cloudflare/workers-sdk/commit/a232ccffe6a2994df5181b6252965a7ba4a0c17a), [`e7f8dc3`](https://github.com/cloudflare/workers-sdk/commit/e7f8dc32465921e0a9a38e8e3deeaf17c04c010a), [`bf9dca8`](https://github.com/cloudflare/workers-sdk/commit/bf9dca85a16c4133d2d200a9e2fc52dcf8917550), [`940ad89`](https://github.com/cloudflare/workers-sdk/commit/940ad89713fa086f23d394570c328716bfb1bd59), [`5d6d521`](https://github.com/cloudflare/workers-sdk/commit/5d6d5218ba0686279e6b67d86592ece16949bf25), [`489b9c5`](https://github.com/cloudflare/workers-sdk/commit/489b9c51550d583d50e262f5905393501c2d6419)]:
+  - wrangler@3.45.0
+  - miniflare@3.20240329.1
+
+## 0.1.12
+
+### Patch Changes
+
+- Updated dependencies [[`0cce21f`](https://github.com/cloudflare/workers-sdk/commit/0cce21ff5b27cc4c227e102eb470b0e0cae455bb), [`02a1091`](https://github.com/cloudflare/workers-sdk/commit/02a109172e60446a8c8e79a2804fdd387c4525a5), [`f69e562`](https://github.com/cloudflare/workers-sdk/commit/f69e5629f8155186e7e890aa38509bb3fbfa704f)]:
+  - wrangler@3.44.0
+
+## 0.1.11
+
+### Patch Changes
+
+- Updated dependencies [[`ef9fbba`](https://github.com/cloudflare/workers-sdk/commit/ef9fbba36444fac665b95bedb2acd1fda494871b), [`91a2150`](https://github.com/cloudflare/workers-sdk/commit/91a2150b9e565d1d6519f635e19f36fc2dec0886)]:
+  - wrangler@3.43.0
+
+## 0.1.10
+
+### Patch Changes
+
+- Updated dependencies [[`77152f3`](https://github.com/cloudflare/workers-sdk/commit/77152f355340d3aac492164fe912a7c5d7a3daeb), [`d994066`](https://github.com/cloudflare/workers-sdk/commit/d994066f255f6851759a055eac3b52a4aa4b83c3)]:
+  - wrangler@3.42.0
+  - miniflare@3.20240329.0
+
+## 0.1.9
+
+### Patch Changes
+
+- Updated dependencies [[`b7a6d9d`](https://github.com/cloudflare/workers-sdk/commit/b7a6d9d422dbe1f09f35b5105a9a58dd425604a7)]:
+  - wrangler@3.41.0
+
+## 0.1.8
+
+### Patch Changes
+
+- Updated dependencies [[`daac6a2`](https://github.com/cloudflare/workers-sdk/commit/daac6a2282c362a79990794dc00baca56ccc3e6e), [`9343714`](https://github.com/cloudflare/workers-sdk/commit/9343714155d5fa71c7415457dd35ab343d047d0f), [`dc0c1dc`](https://github.com/cloudflare/workers-sdk/commit/dc0c1dc527c3ed2f79196f3b0ef44b337833a07a), [`7115568`](https://github.com/cloudflare/workers-sdk/commit/71155680d3675acd6f522e8b312aa63846a076a4), [`c90dd6b`](https://github.com/cloudflare/workers-sdk/commit/c90dd6b8a86238003ac953bd97566f92a206817d), [`b341614`](https://github.com/cloudflare/workers-sdk/commit/b3416145f3fc220aa833e24cbaa1c8612062e2de), [`976adec`](https://github.com/cloudflare/workers-sdk/commit/976adec23e3d993b190faf65f4f06b0508c5a22d), [`3e5a932`](https://github.com/cloudflare/workers-sdk/commit/3e5a932eca2e3e26d135e005967ca36801f27d97), [`fbdca7d`](https://github.com/cloudflare/workers-sdk/commit/fbdca7d93156f9db2a1513573e45f10fac7e57d1), [`47b325a`](https://github.com/cloudflare/workers-sdk/commit/47b325af0df87bcf20d922ff385ae9cd21726863)]:
+  - wrangler@3.40.0
+  - miniflare@3.20240320.1
+
+## 0.1.7
+
+### Patch Changes
+
+- Updated dependencies [[`5bd8db8`](https://github.com/cloudflare/workers-sdk/commit/5bd8db82a64f2c4ffab1b059b240ba6e6eaafde1), [`e11e169`](https://github.com/cloudflare/workers-sdk/commit/e11e1691a0748c5d6520dc6c2d3d796886ea931f), [`7c701bf`](https://github.com/cloudflare/workers-sdk/commit/7c701bf75731646860be10f2515d9944c7e32361)]:
+  - wrangler@3.39.0
+
+## 0.1.6
+
+### Patch Changes
+
+- Updated dependencies [[`7d160c7`](https://github.com/cloudflare/workers-sdk/commit/7d160c7fcaa8097aa3bd8b80b866ec80233be1e9), [`528c011`](https://github.com/cloudflare/workers-sdk/commit/528c011617243d1a290950e76bb88d0986a20f6a), [`7d160c7`](https://github.com/cloudflare/workers-sdk/commit/7d160c7fcaa8097aa3bd8b80b866ec80233be1e9), [`f5e2367`](https://github.com/cloudflare/workers-sdk/commit/f5e2367288e7f57365ef8a1373bbc404bb50a662), [`3be826f`](https://github.com/cloudflare/workers-sdk/commit/3be826f8411ef8d517d572f25a6be38cb8c12cc1), [`528c011`](https://github.com/cloudflare/workers-sdk/commit/528c011617243d1a290950e76bb88d0986a20f6a), [`ba52208`](https://github.com/cloudflare/workers-sdk/commit/ba52208147307608a1233157423e5887203e4547)]:
+  - wrangler@3.38.0
+
+## 0.1.5
+
+### Patch Changes
+
+- Updated dependencies [[`bdc121d`](https://github.com/cloudflare/workers-sdk/commit/bdc121de0a05aaa4716269e2a96b3c4ae3385d8e), [`248a318`](https://github.com/cloudflare/workers-sdk/commit/248a318acac293615327affe35b83018a48dddc9), [`e88ad44`](https://github.com/cloudflare/workers-sdk/commit/e88ad444f2dc54bbf4af4ac8d054ab6cd1af6898), [`9fd7eba`](https://github.com/cloudflare/workers-sdk/commit/9fd7eba3f2b526530b6934a613174541ba321eca)]:
+  - wrangler@3.37.0
+  - miniflare@3.20240320.0
+
+## 0.1.4
+
+### Patch Changes
+
+- Updated dependencies [[`c60fed0`](https://github.com/cloudflare/workers-sdk/commit/c60fed09f3ba3260f182f9d2e6c7c6d0bb123eac), [`e739b7f`](https://github.com/cloudflare/workers-sdk/commit/e739b7fecfb6f3f99a50091be4b7bcd44fdbaa71), [`ac93411`](https://github.com/cloudflare/workers-sdk/commit/ac93411fdb124a784736db704d40592cde227535), [`bfc4282`](https://github.com/cloudflare/workers-sdk/commit/bfc4282de58066d5a9ab07d3e8419ed12b927a96), [`93150aa`](https://github.com/cloudflare/workers-sdk/commit/93150aa0ee51dc3db0c15b6a7126fca11bc2ba0f)]:
+  - wrangler@3.36.0
+
+## 0.1.3
+
+### Patch Changes
+
+- [#5261](https://github.com/cloudflare/workers-sdk/pull/5261) [`4618fb0`](https://github.com/cloudflare/workers-sdk/commit/4618fb0060d5963be0859156437032ae3f5227f7) Thanks [@mrbbot](https://github.com/mrbbot)! - fix: link [migration guide](https://developers.cloudflare.com/workers/testing/vitest-integration/get-started/migrate-from-miniflare-2/) when `environment: "miniflare"` is specified in Vitest configuration
+
+- Updated dependencies [[`133a190`](https://github.com/cloudflare/workers-sdk/commit/133a1907087741a4ea3cda7f53ce93919168e8f8), [`0a86050`](https://github.com/cloudflare/workers-sdk/commit/0a860507e49329d0e140de47830d670397e08c13), [`e1f2576`](https://github.com/cloudflare/workers-sdk/commit/e1f2576e1511a53786cebcde12d8c2cf4b3ce566), [`1720f0a`](https://github.com/cloudflare/workers-sdk/commit/1720f0a12a6376093b3c5799d74f47c522ae8571), [`a676f55`](https://github.com/cloudflare/workers-sdk/commit/a676f55a457a8b34b1c80f666f615eb258ad58c4), [`8f79981`](https://github.com/cloudflare/workers-sdk/commit/8f799812a3de1c93fb4dcb7a2a89e60c2c0173cd)]:
+  - wrangler@3.35.0
+  - miniflare@3.20240314.0
+
+## 0.1.2
+
+### Patch Changes
+
+- Updated dependencies [[`a0768bc`](https://github.com/cloudflare/workers-sdk/commit/a0768bcc9d76be8a88fe3e1aa45f3b3805da3df6)]:
+  - wrangler@3.34.2
+
+## 0.1.1
+
+### Patch Changes
+
+- Updated dependencies [[`2e50d51`](https://github.com/cloudflare/workers-sdk/commit/2e50d51632dfe905bd32de8176231bb256c88dab)]:
+  - miniflare@3.20240304.2
+  - wrangler@3.34.1
+
+## 0.1.0
+
+### Minor Changes
+
+- [#5241](https://github.com/cloudflare/workers-sdk/pull/5241) [`ca891e7`](https://github.com/cloudflare/workers-sdk/commit/ca891e783a41053770f278b91f9001c477550743) Thanks [@mrbbot](https://github.com/mrbbot)! - feature: implement Workers Vitest integration
+
+  Introducing the new Workers Vitest integration! The `@cloudflare/vitest-pool-workers` package allows you to write unit and integration tests using Vitest that run inside the Workers runtime. Refer to the [documentation](https://developers.cloudflare.com/workers/testing/vitest-integration/) and [examples](https://github.com/cloudflare/workers-sdk/tree/main/fixtures/vitest-pool-workers-examples/) for more information.
+
+### Patch Changes
+
+- Updated dependencies [[`03484c2`](https://github.com/cloudflare/workers-sdk/commit/03484c2d64f42a2820feeec9076dc3f210baf4f9), [`29e8151`](https://github.com/cloudflare/workers-sdk/commit/29e8151bc2235bd13074584df5f90187955123d2), [`4730b6c`](https://github.com/cloudflare/workers-sdk/commit/4730b6c087080d79838d3fd86480d8aff693834a), [`bd935cf`](https://github.com/cloudflare/workers-sdk/commit/bd935cfdf1bebfff53b1817d475b1d36eccec9c0)]:
+  - wrangler@3.34.0
---
+++ packages/wrangler/src/r2/helpers.ts
@@ -0,0 +1,516 @@
+import { Miniflare } from "miniflare";
+import { fetchResult } from "../cfetch";
+import { fetchR2Objects } from "../cfetch/internal";
+import { getLocalPersistencePath } from "../dev/get-local-persistence-path";
+import { buildPersistOptions } from "../dev/miniflare";
+import { UserError } from "../errors";
+import { logger } from "../logger";
+import { getQueue } from "../queues/client";
+import type { Config } from "../config";
+import type { getQueueByID } from "../queues/client";
+import type { ApiCredentials } from "../user";
+import type { R2Bucket } from "@cloudflare/workers-types/experimental";
+import type { ReplaceWorkersTypes } from "miniflare";
+import type { Readable } from "node:stream";
+import type { ReadableStream } from "node:stream/web";
+import type { HeadersInit } from "undici";
+
+/**
+ * Information about a bucket, returned from `listR2Buckets()`.
+ */
+export interface R2BucketInfo {
+	name: string;
+	creation_date: string;
+}
+
+/**
+ * Fetch a list of all the buckets under the given `accountId`.
+ */
+export async function listR2Buckets(
+	accountId: string,
+	jurisdiction?: string
+): Promise<R2BucketInfo[]> {
+	const headers: HeadersInit = {};
+	if (jurisdiction !== undefined) {
+		headers["cf-r2-jurisdiction"] = jurisdiction;
+	}
+	const results = await fetchResult<{
+		buckets: R2BucketInfo[];
+	}>(`/accounts/${accountId}/r2/buckets`, { headers });
+	return results.buckets;
+}
+
+/**
+ * Create a bucket with the given `bucketName` within the account given by `accountId`.
+ *
+ * A 400 is returned if the account already owns a bucket with this name.
+ * A bucket must be explicitly deleted to be replaced.
+ */
+export async function createR2Bucket(
+	accountId: string,
+	bucketName: string,
+	jurisdiction?: string
+): Promise<void> {
+	const headers: HeadersInit = {};
+	if (jurisdiction !== undefined) {
+		headers["cf-r2-jurisdiction"] = jurisdiction;
+	}
+	return await fetchResult<void>(`/accounts/${accountId}/r2/buckets`, {
+		method: "POST",
+		body: JSON.stringify({ name: bucketName }),
+		headers,
+	});
+}
+
+/**
+ * Delete a bucket with the given name
+ */
+export async function deleteR2Bucket(
+	accountId: string,
+	bucketName: string,
+	jurisdiction?: string
+): Promise<void> {
+	const headers: HeadersInit = {};
+	if (jurisdiction !== undefined) {
+		headers["cf-r2-jurisdiction"] = jurisdiction;
+	}
+	return await fetchResult<void>(
+		`/accounts/${accountId}/r2/buckets/${bucketName}`,
+		{ method: "DELETE", headers }
+	);
+}
+
+export function bucketAndKeyFromObjectPath(objectPath = ""): {
+	bucket: string;
+	key: string;
+} {
+	const match = /^([^/]+)\/(.*)/.exec(objectPath);
+	if (match === null) {
+		throw new UserError(
+			`The object path must be in the form of {bucket}/{key} you provided ${objectPath}`
+		);
+	}
+
+	return { bucket: match[1], key: match[2] };
+}
+
+/**
+ * Downloads an object
+ */
+export async function getR2Object(
+	accountId: string,
+	bucketName: string,
+	objectName: string,
+	jurisdiction?: string
+): Promise<ReadableStream | null> {
+	const headers: HeadersInit = {};
+	if (jurisdiction !== undefined) {
+		headers["cf-r2-jurisdiction"] = jurisdiction;
+	}
+	const response = await fetchR2Objects(
+		`/accounts/${accountId}/r2/buckets/${bucketName}/objects/${objectName}`,
+		{
+			method: "GET",
+			headers,
+		}
+	);
+
+	return response === null ? null : response.body;
+}
+
+/**
+ * Uploads an object
+ */
+export async function putR2Object(
+	accountId: string,
+	bucketName: string,
+	objectName: string,
+	object: Readable | ReadableStream | Buffer,
+	options: Record<string, unknown>,
+	jurisdiction?: string
+): Promise<void> {
+	const headerKeys = [
+		"content-length",
+		"content-type",
+		"content-disposition",
+		"content-encoding",
+		"content-language",
+		"cache-control",
+		"expires",
+	];
+	const headers: HeadersInit = {};
+	for (const key of headerKeys) {
+		const value = options[key] || "";
+		if (value && typeof value === "string") headers[key] = value;
+	}
+	if (jurisdiction !== undefined) {
+		headers["cf-r2-jurisdiction"] = jurisdiction;
+	}
+
+	const result = await fetchR2Objects(
+		`/accounts/${accountId}/r2/buckets/${bucketName}/objects/${objectName}`,
+		{
+			body: object,
+			headers,
+			method: "PUT",
+			duplex: "half",
+		}
+	);
+	if (result === null) {
+		throw new UserError("The specified bucket does not exist.");
+	}
+}
+/**
+ * Delete an Object
+ */
+export async function deleteR2Object(
+	accountId: string,
+	bucketName: string,
+	objectName: string,
+	jurisdiction?: string
+): Promise<void> {
+	const headers: HeadersInit = {};
+	if (jurisdiction !== undefined) {
+		headers["cf-r2-jurisdiction"] = jurisdiction;
+	}
+	await fetchR2Objects(
+		`/accounts/${accountId}/r2/buckets/${bucketName}/objects/${objectName}`,
+		{ method: "DELETE", headers }
+	);
+}
+
+export async function usingLocalBucket<T>(
+	persistTo: string | undefined,
+	configPath: string | undefined,
+	bucketName: string,
+	closure: (
+		namespace: ReplaceWorkersTypes<R2Bucket>,
+		mf: Miniflare
+	) => Promise<T>
+): Promise<T> {
+	const persist = getLocalPersistencePath(persistTo, configPath);
+	const persistOptions = buildPersistOptions(persist);
+	const mf = new Miniflare({
+		modules: true,
+		// TODO(soon): import `reduceError()` from `miniflare:shared`
+		script: `
+		function reduceError(e) {
+			return {
+				name: e?.name,
+				message: e?.message ?? String(e),
+				stack: e?.stack,
+				cause: e?.cause === undefined ? undefined : reduceError(e.cause),
+			};
+		}
+		export default {
+			async fetch(request, env, ctx) {
+				try {
+					if (request.method !== "PUT") return new Response(null, { status: 405 });
+					const url = new URL(request.url);
+					const key = url.pathname.substring(1);
+					const optsHeader = request.headers.get("Wrangler-R2-Put-Options");
+					const opts = JSON.parse(optsHeader);
+					await env.BUCKET.put(key, request.body, opts);
+					return new Response(null, { status: 204 });
+				} catch (e) {
+					const error = reduceError(e);
+					return Response.json(error, {
+						status: 500,
+						headers: { "MF-Experimental-Error-Stack": "true" },
+					});
+				}
+			}
+		}`,
+		...persistOptions,
+		r2Buckets: { BUCKET: bucketName },
+	});
+	const bucket = await mf.getR2Bucket("BUCKET");
+	try {
+		return await closure(bucket, mf);
+	} finally {
+		await mf.dispose();
+	}
+}
+
+type SippyConfig = {
+	source:
+		| { provider: "aws"; region: string; bucket: string }
+		| { provider: "gcs"; bucket: string };
+	destination: {
+		provider: "r2";
+		account: string;
+		bucket: string;
+		accessKeyId: string;
+	};
+};
+
+/**
+ * Retreive the sippy upstream bucket for the bucket with the given name
+ */
+export async function getR2Sippy(
+	accountId: string,
+	bucketName: string,
+	jurisdiction?: string
+): Promise<SippyConfig> {
+	const headers: HeadersInit = {};
+	if (jurisdiction !== undefined) {
+		headers["cf-r2-jurisdiction"] = jurisdiction;
+	}
+	return await fetchResult(
+		`/accounts/${accountId}/r2/buckets/${bucketName}/sippy`,
+		{ method: "GET", headers }
+	);
+}
+
+/**
+ * Disable sippy on the bucket with the given name
+ */
+export async function deleteR2Sippy(
+	accountId: string,
+	bucketName: string,
+	jurisdiction?: string
+): Promise<void> {
+	const headers: HeadersInit = {};
+	if (jurisdiction !== undefined) {
+		headers["cf-r2-jurisdiction"] = jurisdiction;
+	}
+	return await fetchResult(
+		`/accounts/${accountId}/r2/buckets/${bucketName}/sippy`,
+		{ method: "DELETE", headers }
+	);
+}
+
+export type SippyPutParams = {
+	source:
+		| {
+				provider: "aws";
+				region: string;
+				bucket: string;
+				accessKeyId: string;
+				secretAccessKey: string;
+		  }
+		| {
+				provider: "gcs";
+				bucket: string;
+				clientEmail: string;
+				privateKey: string;
+		  };
+	destination: {
+		provider: "r2";
+		accessKeyId: string;
+		secretAccessKey: string;
+	};
+};
+
+/**
+ * Enable sippy on the bucket with the given name
+ */
+export async function putR2Sippy(
+	accountId: string,
+	bucketName: string,
+	params: SippyPutParams,
+	jurisdiction?: string
+): Promise<void> {
+	const headers: HeadersInit = {
+		"Content-Type": "application/json",
+	};
+	if (jurisdiction !== undefined) {
+		headers["cf-r2-jurisdiction"] = jurisdiction;
+	}
+	return await fetchResult(
+		`/accounts/${accountId}/r2/buckets/${bucketName}/sippy`,
+		{ method: "PUT", body: JSON.stringify(params), headers }
+	);
+}
+
+export const R2EventableOperations = [
+	"PutObject",
+	"DeleteObject",
+	"CompleteMultipartUpload",
+	"CopyObject",
+] as const;
+export type R2EventableOperation = typeof R2EventableOperations[number];
+
+export const actionsForEventCategories: Record<
+	"object-create" | "object-delete",
+	R2EventableOperation[]
+> = {
+	"object-create": ["PutObject", "CompleteMultipartUpload", "CopyObject"],
+	"object-delete": ["DeleteObject"],
+};
+export type R2EventType = keyof typeof actionsForEventCategories;
+export const eventCategoryByAction: Record<R2EventableOperation, R2EventType> =
+	{
+		PutObject: "object-create",
+		CompleteMultipartUpload: "object-create",
+		CopyObject: "object-create",
+		DeleteObject: "object-delete",
+	};
+type NotificationRule = {
+	prefix?: string;
+	suffix?: string;
+	actions: R2EventableOperation[];
+};
+export type DetailID = string;
+type QueueID = string;
+type BucketName = string;
+export type NotificationDetail = Record<
+	DetailID, // This is the detail ID that identifies this config
+	{ queue: QueueID; rules: NotificationRule[] }
+>;
+export type GetNotificationConfigResponse = Record<
+	BucketName,
+	NotificationDetail
+>;
+// This type captures the shape of the data expected by EWC API.
+export type PutNotificationRequestBody = {
+	// `jurisdiction` is included here for completeness, but until Queues
+	// supports jurisdictions, then this command will not send anything to do
+	// with jurisdictions.
+	jurisdiction?: string;
+	rules: NotificationRule[];
+};
+
+export function eventNotificationHeaders(
+	apiCredentials: ApiCredentials
+): HeadersInit {
+	const headers: HeadersInit = {
+		"Content-Type": "application/json",
+	};
+
+	if ("apiToken" in apiCredentials) {
+		headers["Authorization"] = `Bearer ${apiCredentials.apiToken}`;
+	} else {
+		headers["X-Auth-Key"] = apiCredentials.authKey;
+		headers["X-Auth-Email"] = apiCredentials.authEmail;
+	}
+	return headers;
+}
+
+// Reformat the per-bucket get-notification response into a format
+// suitable for `logger.table()`
+export async function tableFromNotificationGetResponse(
+	config: Pick<Config, "account_id">,
+	response: GetNotificationConfigResponse[BucketName],
+	// We're injecting this parameter because it makes testing easier,
+	// relative to mocking.
+	queueIdentifier: typeof getQueueByID
+): Promise<
+	{
+		queue_name: string;
+		prefix: string;
+		suffix: string;
+		event_type: string;
+	}[]
+> {
+	const reducer = async ([_, { queue, rules }]: [
+		DetailID,
+		NotificationDetail[DetailID]
+	]) => {
+		const queueResp = await queueIdentifier(config, queue);
+		const rows = [];
+		for (const { prefix = "", suffix = "", actions } of rules) {
+			rows.push({
+				queue_name: queueResp.queue_name,
+				prefix,
+				suffix,
+				event_type: Array.from(
+					actions.reduce((acc, action) => {
+						acc.add(eventCategoryByAction[action]);
+						return acc;
+					}, new Set<R2EventType>())
+				).join(","),
+			});
+		}
+		return rows;
+	};
+
+	let tableOutput: {
+		queue_name: string;
+		prefix: string;
+		suffix: string;
+		event_type: string;
+	}[] = [];
+	for (const entry of Object.entries(response)) {
+		const result = await reducer(entry);
+		tableOutput = tableOutput.concat(...result);
+	}
+	return tableOutput;
+}
+
+export async function getEventNotificationConfig(
+	apiCredentials: ApiCredentials,
+	accountId: string,
+	bucketName: string
+): Promise<GetNotificationConfigResponse> {
+	const headers = eventNotificationHeaders(apiCredentials);
+	logger.log(`Fetching notification configuration for bucket ${bucketName}...`);
+	return await fetchResult<GetNotificationConfigResponse>(
+		`/accounts/${accountId}/event_notifications/r2/${bucketName}/configuration`,
+		{ method: "GET", headers }
+	);
+}
+
+/** Construct & transmit notification configuration to EWC.
+ *
+ * On success, receive HTTP 200 response with a body like:
+ * { event_notification_detail_id: string }
+ *
+ * Possible status codes on failure:
+ * - 400 Bad Request - Either:
+ * 		- Uploaded configuration is invalid
+ * 		- Communication with either R2-gateway-worker or queue-broker-worker fails
+ * - 409 Conflict - A configuration between the bucket and queue already exists
+ * */
+export async function putEventNotificationConfig(
+	config: Config,
+	apiCredentials: ApiCredentials,
+	accountId: string,
+	bucketName: string,
+	queueName: string,
+	eventTypes: R2EventType[],
+	prefix?: string,
+	suffix?: string
+): Promise<{ event_notification_detail_id: string }> {
+	const queue = await getQueue(config, queueName);
+	const headers = eventNotificationHeaders(apiCredentials);
+	let actions: R2EventableOperation[] = [];
+
+	for (const et of eventTypes) {
+		actions = actions.concat(actionsForEventCategories[et]);
+	}
+
+	const body: PutNotificationRequestBody = {
+		rules: [{ prefix, suffix, actions }],
+	};
+	const ruleFor = eventTypes.map((et) =>
+		et === "object-create" ? "creation" : "deletion"
+	);
+	logger.log(
+		`Creating event notification rule for object ${ruleFor.join(
+			" and "
+		)} (${actions.join(",")})`
+	);
+	return await fetchResult<{ event_notification_detail_id: string }>(
+		`/accounts/${accountId}/event_notifications/r2/${bucketName}/configuration/queues/${queue.queue_id}`,
+		{ method: "PUT", body: JSON.stringify(body), headers }
+	);
+}
+
+export async function deleteEventNotificationConfig(
+	config: Config,
+	apiCredentials: ApiCredentials,
+	accountId: string,
+	bucketName: string,
+	queueName: string
+): Promise<null> {
+	const queue = await getQueue(config, queueName);
+	const headers = eventNotificationHeaders(apiCredentials);
+	logger.log(
+		`Disabling event notifications for "${bucketName}" to queue ${queueName}...`
+	);
+	return await fetchResult<null>(
+		`/accounts/${accountId}/event_notifications/r2/${bucketName}/configuration/queues/${queue.queue_id}`,
+		{ method: "DELETE", headers }
+	);
+}
