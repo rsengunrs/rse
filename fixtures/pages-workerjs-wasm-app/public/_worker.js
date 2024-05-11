aa1851e69f69a039b0ed05878673e226494d6e32
rsengunrs <167508541+rsengunrs@users.noreply.github.com>
2024-05-11 05:34:39 +0300

    Initialize penalosa/nodejs-compat-functions-build

---
+++ fixtures/pages-workerjs-wasm-app/public/_worker.js
@@ -0,0 +1,28 @@
+import multiply from "./../wasm/multiply.wasm";
+import html from "./../external-modules/meaning-of-life.html";
+
+export default {
+	async fetch(request, env) {
+		const url = new URL(request.url);
+		const multiplyModule = await WebAssembly.instantiate(multiply);
+
+		if (url.pathname === "/meaning-of-life-wasm") {
+			return new Response(
+				`[.wasm]: The meaning of life is ${multiplyModule.exports.multiply(
+					7,
+					3
+				)}`
+			);
+		}
+
+		if (url.pathname === "/meaning-of-life-html") {
+			return new Response(html, {
+				headers: {
+					"Content-Type": "text/html",
+				},
+			});
+		}
+
+		return env.ASSETS.fetch(request);
+	},
+};
